f5ba94569ea0508e05ac3da4313f29ea4707a733
Cobbleopolis <agounimon@yahoo.com>
2015-07-18 00:16:19 -0400

    Initial commit

---
+++ src/Gud.scala
@@ -0,0 +1,9 @@
+object Gud {
+    /**
+     * Main method
+     * @param args Arguments passed to the game
+     */
+    def main(args: Array[String]): Unit = {
+        println("YOU WANA FUCKIN FIGHT! #FUKINREKTM8!")
+    }
+}
